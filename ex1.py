5248933b3e83b4363557f84bfcf8d5f539c8fc9e
xia0m <alexma325@gmail.com>
2019-04-16 23:51:05 -0700

    added README.md

---
+++ ex1.py
@@ -0,0 +1,19 @@
+print("Hello World")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# 1. Make your script print another line.
+
+print("Line1\nLine2")
+
+# 2. Make your script print only one of the lines
+
+print("Line3")
+
+# 3. Put a # (octothorpe) character at the beginning of a line.
+
+# print("Line4")
